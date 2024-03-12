83e09c2462f8fcc4037ef766c88a37e93784dce1
Jarrett <jarrettcrump@gmail.com>
2024-03-11 22:07:44 -0500

    File handler initial setup

--- OceanMotion Control Center/FileHandler.cs
+++ OceanMotion Control Center/FileHandler.cs
@@ -1,5 +1,38 @@
+using System.IO;
+
 namespace OceanMotion;
 
 public class FileHandler() {
     
+    private StreamReader sr;
+    private StreamWriter sw;
+
+    public int ValidateFile(String path) {
+        return ReadFile(path);
+    }
+
+    private int ReadFile(String path) {
+        String line1;
+        String line2;
+        try {
+            // REFACTOR***
+            sr = new StreamReader(path);
+            line = sr.ReadLine();
+            while ((line = sr.ReadLine()) != null) {
+                Validate(line);
+            }
+        }
+        catch(FileNotFoundException e) {
+            // Log & display error
+            return -1;
+        }
+        catch(IOException e) { // Triggered when file is empty I believe
+            // Log & display error
+            return -2;
+        }
+        return 0;
+    }
+    private int Validate(String line) {
+        return 0;
+    }
 }
